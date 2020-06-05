dc97fc6dcb0f567657efdf847bbab5759773fcc9
Britton W <britton.wolfe1999@gmail.com>
2020-06-05 12:58:51 -0400

    added navigation links and a copy text functionality

--- static/js/clip.js
+++ static/js/clip.js
@@ -2,9 +2,10 @@ let copy = document.querySelector("#copy");
 
 function copyFunc() {
 	let range = document.createRange();
-	range.selectNode(document.querySelector("#code"));
+	range.selectNode(document.querySelector("code"));
 	window.getSelection().addRange(range);
 	document.execCommand('copy');
+	window.getSelection().empty();
 }
 
 copy.addEventListener('click', copyFunc);
